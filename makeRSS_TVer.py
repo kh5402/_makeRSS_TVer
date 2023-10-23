4afe8766841b29dbb088bf726536993d9a703138
kh <127534515+kh5402@users.noreply.github.com>
2023-10-23 15:36:21 +0900

    Update makeRSS_TVer.py

--- makeRSS_TVer.py
+++ makeRSS_TVer.py
@@ -5,7 +5,6 @@ from datetime import datetime
 import pytz
 import os
 import re
-import logging
 
 def main():
     output_file = "makeRSS_TVer.xml"
@@ -24,18 +23,18 @@ def main():
         ET.SubElement(channel, "description").text = "TVer"
         ET.SubElement(channel, "link").text = "https://tver.jp/newer"
 
-    logging.debug("Initialized RSS feed.")
+    print("Initialized RSS feed.")
     
     url = "https://tver.jp/newer"
     response = requests.get(url)
     pageContent = response.text
 
-    logging.debug("Fetched webpage content.")
-    logging.debug(pageContent)
+    print("Fetched webpage content.")
+    print(pageContent)
         
     # 正規表現で取得する部分
     articles = re.findall(r'<div class="newer-page-main_spEpisodeWrapper__huS6z">.*?</div></div></div></div>', pageContent)
-    logging.debug(f"Found {len(articles)} articles.")
+    print(f"Found {len(articles)} articles.")
     
     for article in articles:
         link_match = re.search(r'href="(/episodes/[^"]+)"', article)
@@ -62,7 +61,7 @@ def main():
         ET.SubElement(item, "link").text = link
         ET.SubElement(item, "pubDate").text = date_now
 
-    logging.debug("Added new item to RSS.")
+    print("Added new item to RSS.")
                 
     # 整形して保存
     xml_str = ET.tostring(root)
@@ -77,7 +76,7 @@ def main():
     with open(output_file, "w") as f:
         f.write(xml_pretty_str)
 
-    logging.debug("Saved RSS feed.")
+    print("Saved RSS feed.")
 
 if __name__ == "__main__":
     main()
